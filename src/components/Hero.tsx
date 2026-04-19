f9010067a8f16655e4fe995dee2a13362a7cef16
adm37 <ademsade@gmail.com>
2026-04-19 23:18:23 +0200

    Update hero video URL to https source

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,16 +5,17 @@ import { ArrowRight } from "lucide-react";
 export default function Hero() {
   return (
     <section className="relative h-screen w-full flex items-center justify-center overflow-hidden">
-      {/* YouTube Background Video */}
+      {/* Background Video */}
       <div className="absolute inset-0 w-full h-full pointer-events-none">
-        <iframe
-          className="absolute top-1/2 left-1/2 w-[100vw] h-[56.25vw] min-h-[100vh] min-w-[177.77vh] -translate-x-1/2 -translate-y-1/2"
-          src="https://www.youtube.com/embed/UFJjKL6Eduo?autoplay=1&mute=1&loop=1&playlist=UFJjKL6Eduo&controls=0&showinfo=0&rel=0&modestbranding=1&iv_load_policy=3"
-          title="Epoxy Floor Video Background"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        <video
+          className="absolute inset-0 h-full w-full object-cover"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source src="https://klasseepoxy.nl/klasse%20epoxy%20video.mp4" type="video/mp4" />
+        </video>
       </div>
 
       {/* Overlay */}
